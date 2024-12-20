4198189e8269a4cf87ee9503d9bf256a2bd6aaf6
taihuynh <nhattaihuynh@gmail.com>
2024-12-20 22:00:10 +0700

    test

--- neo4j-example/src/main/java/com/example/neo4jexample/Neo4jExampleApplication.java
+++ neo4j-example/src/main/java/com/example/neo4jexample/Neo4jExampleApplication.java
@@ -2,10 +2,13 @@ package com.example.neo4jexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class Neo4jExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(Neo4jExampleApplication.class, args);
+        log.info("Neo4j Example Application Started!");
     }
 }
